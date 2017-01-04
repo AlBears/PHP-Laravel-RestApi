fe83457682c339f57e285a6da5da5c3eb9c229b8
Alexey Sidorovitch <sidorovitch@gmail.com>
2017-01-04 16:25:22 +0300

    get request

--- config/app.php
+++ config/app.php
@@ -178,6 +178,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        App\Providers\v1\FlightServiceProvider::class,
+
     ],
 
     /*
